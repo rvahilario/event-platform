c5ba6fe68d7ba7e129abc28ae54be482c51aad6b
Renan Hilario <rvahilario@gmail.com>
2022-06-30 00:56:12 -0300

    refact: :recycle: updating and move lessons query to sidebar comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
 import { gql, useQuery } from '@apollo/client';
 import { Event } from './pages/Event';
 
-const GET_LESSONS_QUERY = gql`
-	query {
-		lessons {
-			id
-			title
-		}
-	}
-`;
-
-interface Lesson {
-	id: string;
-	title: string;
-}
-
 const App = () => {
-	const { data } = useQuery<{ lessons: Array<Lesson> }>(GET_LESSONS_QUERY);
-
 	return (
 		<Event />
 		// <ul>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,31 @@
+import { gql, useQuery } from '@apollo/client';
 import { LessonCard } from './LessonCard';
 
+const GET_LESSONS_QUERY = gql`
+	query {
+		lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+			id
+			title
+			availableAt
+			lessonType
+			slug
+		}
+	}
+`;
+
+interface GetLessonsQueryResponse {
+	lessons: Array<{
+		id: string;
+		title: string;
+		availableAt: string;
+		lessonType: 'live' | 'class';
+		slug: string;
+	}>;
+}
+
 export const Sidebar = () => {
+	const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+
 	return (
 		<aside className="w-[348px] bg-gray-700 border-l border-gray-600 p-6">
 			<span className="pb-6 mb-6 text-2xl font-bold border-b border-gray-600 block">
@@ -8,9 +33,15 @@ export const Sidebar = () => {
 			</span>
 
 			<div className="flex flex-col gap-8">
-				<LessonCard />
-				<LessonCard />
-				<LessonCard />
+				{data?.lessons.map((lesson) => (
+					<LessonCard
+						key={lesson.id}
+						availableAt={new Date(lesson.availableAt)}
+						title={lesson.title}
+						type={lesson.lessonType}
+						slug={lesson.slug}
+					/>
+				))}
 			</div>
 		</aside>
 	);
