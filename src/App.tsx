8a55fa705a3c034f260716fe40c4f0c7053f28c8
Renan Hilario <rvahilario@gmail.com>
2022-06-30 03:04:24 -0300

    feat: :star2: setting Router and moving providers from main to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { gql, useQuery } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
 import { Event } from './pages/Event';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './lib/apollo';
 
 const App = () => {
 	return (
-		<Event />
+		<ApolloProvider client={client}>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+		</ApolloProvider>
+		// <Event />
+
 		// <ul>
 		// 	{data?.lessons.map((lesson: Lesson) => {
 		// 		return <li key={lesson.id}>{lesson.title}</li>;
---
+++ src/Router.tsx
@@ -0,0 +1,9 @@
+import { Route, Routes } from 'react-router-dom';
+import { Event } from './pages/Event';
+
+export const Router = () => (
+	<Routes>
+		<Route path="/" element={<Event />} />
+		<Route path="/event" element={<Event />} />
+	</Routes>
+);
