802ecaca6ae0c1e46955df336aff9b6379c30d2b
Renan Hilario <rvahilario@gmail.com>
2022-06-30 02:14:50 -0300

    feat: :star2: add description space to video comp

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -6,6 +6,40 @@ export const Video = () => {
 					<div className="flex h-full justify-center items-center">Video</div>
 				</div>
 			</div>
+
+			<div className="p-8 max-w-[1100px] mx-auto">
+				<div className="flex items-start gap-16">
+					<div className="flex-1">
+						<h1 className="text-2xl font-bold">
+							Class 01 - Opening of Ignite Lab
+						</h1>
+						<p className="mt-4 text-gray-200 leading-relaxed">
+							Esse adipisicing amet id officia voluptate nostrud proident et sit
+							nisi. Adipisicing proident enim incididunt adipisicing et commodo
+							eu aute qui dipisicing. Aute sit aute cupidatat consequat ipsum id
+							et do est pariatur ea elit. Voluptate sint duis exercitation
+							tempor ipsum dolor laboris occaecat sint mollit eiusmod ipsum et.
+							Cupidatat ea eu incididunt voluptate sunt deserunt aliqua laboris
+							adipisicing adipisicing.
+						</p>
+
+						<div className="flex items-center gap-4 mt-6">
+							<img
+								src="https://github.com/rvahilario.png"
+								alt="Professor avatar"
+								className="h-16 w-16 rounded-full border-2 border-blue-500"
+							/>
+
+							<div className="leading-relaxed">
+								<strong className="font-bold text-2xl block">
+									Nome professor
+								</strong>
+								<span className="text-gray-200 text-sm block">Biografia</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
