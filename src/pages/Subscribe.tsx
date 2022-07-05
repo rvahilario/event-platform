1b861ef949b458d79fdfc10a7af59add93a7fa50
Renan Hilario <rvahilario@gmail.com>
2022-07-05 01:43:30 -0300

    feat: :star2: create Subscribe page and add background composition

---
+++ src/pages/Subscribe.tsx
@@ -0,0 +1,11 @@
+export const Subscribe = () => {
+	return (
+		<div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+			<img
+				src="/src/assets/code-mockup.png"
+				alt="logo"
+				className="mt-[-55px]"
+			/>
+		</div>
+	);
+};
